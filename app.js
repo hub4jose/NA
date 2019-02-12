7c8a9858fcd56e093c12ff4290fd4aa8c81c45ee
hub4jose <jnakarmi123@gmail.com>
2019-02-12 07:51:58 -0500

    upt4

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var http =require('http');
 
-var hostname = '127.0.0.1';
+//var hostname = '127.0.0.1';
 var port = process.env.PORT || 1337;
 
 http.createServer(function(request, response){
   response.writeHead(200,{'Content-Type': 'text/plain'});
   response.end('Hello World\n');
-}).listen(port, hostname, function(){
-   console.log('Server runnning at http://' + hostname + ':' + port + '/');
+}).listen(port, function(){
+   console.log('Server runnning at http://' + port + '/');
 });
